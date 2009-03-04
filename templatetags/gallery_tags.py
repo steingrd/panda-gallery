818b9f9a00464d4b134db145cc68ec0f417cde18
Steingrim Dovland <steingrd@ifi.uio.no>
2009-03-04 22:31:28 +0100

    photos now link to photo at flickr using a custom filter

--- templatetags/gallery_tags.py
+++ templatetags/gallery_tags.py
@@ -7,5 +7,10 @@ register = template.Library()
 def as_flickr_link_filter(value):
     return "http://farm%(farm)s.static.flickr.com/%(server)s/%(id)s_%(secret)s_s.jpg" % value.attrib
 
-    
+@register.filter(name='as_flickr_user_link')
+def as_flickr_user_link_filter(value):
+    return "http://www.flickr.com/photos/%(owner)s/%(id)s" % value.attrib
 
+@register.filter(name='as_title')
+def as_title_filter(value):
+    return value.attrib['ownername'] + ' - ' + value.attrib['title']
